4ef241a88e99a08fd1c3c9f447d77cb737d4e52e
Yuu Yonamine <yu_yonamine@ex-ex.co.jp>
2023-02-27 16:01:04 +0900

    constructer

--- src/access-modifiers.ts
+++ src/access-modifiers.ts
@@ -2,11 +2,13 @@ export {};
 
 class Person {
   public name: string;
-  private age: number;
+  //private age: number;
+  protected age: number;
   protected nationality: string;
   constructor(name: string,age: number,nationality: string) {
    this.name =name;
    this.age = age;
+   this.nationality = nationality;
   }
 
   profile(): string {
@@ -18,9 +20,12 @@ class Android extends Person{
 constructor(name: string, age: number, nationality: string){
 super(name,age,nationality)
 }
+profile(): string{
+  return `name: ${this.name},age: ${this.age},nationality: ${this.nationality}`;
 
 }
-let taro = new Person("taro",30,"japan");
+}
+let taro = new Person('taro',30,'japan');
 console.log(taro.profile());
 console.log(taro.name);
 //console.log(taro.age);
